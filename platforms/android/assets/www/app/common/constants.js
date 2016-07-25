003302d4e6aaf12ac66fba7229784c57e7b1cb68
Akhil Kulkarni <akhil16.4.93@gmail.com>
2016-07-26 00:06:37 +0530

    oauth changes

---
+++ platforms/android/assets/www/app/common/constants.js
@@ -0,0 +1,7 @@
+var GOOGLE_CLIENT_ID = '383591290055-qjtae623tchqqcluttl7u7fj6ba2jo1r.apps.googleusercontent.com';
+var GOOGLE_SECRET_KEY = '70WHVR_UKKx0j8y785z84ZBv';
+/*if(ionic.Platform.isAndroid())
+	GOOGLE_CLIENT_ID = "383591290055-ktr31mnja3uqc321u4opvd7mljgva2e4.apps.googleusercontent.com";
+else if(ionic.Platform.isIOS())
+	GOOGLE_CLIENT_ID = "383591290055-larrfelqa3c6detofm4kn2q49p0j09f7.apps.googleusercontent.com";*/
+console.log("google client id: " + GOOGLE_CLIENT_ID);
--- www/app/login/login.ctrl.js
+++ www/app/login/login.ctrl.js
@@ -38,22 +38,22 @@ loginModule.service('LoginService',['$http', '$state', function($http, $state){
 			console.log("in sign in google");
 			var requestToken = null;
 			var accessToken = null;
-			var clientId = null;
-
-			if(ionic.Platform.isAndroid())
-				clientId = "383591290055-ktr31mnja3uqc321u4opvd7mljgva2e4.apps.googleusercontent.com";
-			else if(ionic.Platform.isIOS())
-				clientId = "383591290055-larrfelqa3c6detofm4kn2q49p0j09f7.apps.googleusercontent.com";
+			var clientId = GOOGLE_CLIENT_ID;
 
 			$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-			var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/urlshortener&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+			var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/urlshortener email&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
 			ref.addEventListener('loadstart', function(event) {
 				if((event.url).startsWith("http://localhost/callback")) {
 					requestToken = (event.url).split("code=")[1];
-					$http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + "" + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+					$http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + GOOGLE_SECRET_KEY + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
 					.success(function(data) {
 						accessToken = data.access_token;
-						$state.go("profile");
+						$http({method: "post", url: "https://www.googleapis.com/oauth2/v3/tokeninfo", data: "access_token=" + accessToken }).success(
+							function(tokenInfo){
+								alert(JSON.stringify(tokenInfo));
+								$state.go("profile");
+							}
+						)
 					})
 					.error(function(data, status) {
 						alert("ERROR: " + JSON.stringify(data));
